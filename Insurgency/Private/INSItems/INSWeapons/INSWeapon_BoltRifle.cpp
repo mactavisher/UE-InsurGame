e68e4c87fb87e1965a8ea137fcb4ca0eeb90ae0a
Mactavish <1434117225@qq.com>
2021-07-09 18:01:52 +0800

    add bolt type rifle which wll have a cycle round reload or shot

---
+++ Insurgency/Private/INSItems/INSWeapons/INSWeapon_BoltRifle.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "INSItems/INSWeapons/INSWeapon_BoltRifle.h"
+
---
+++ Insurgency/Public/INSItems/INSWeapons/INSWeapon_BoltRifle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "INSItems/INSWeapons/INSWeaponBase.h"
+#include "INSWeapon_BoltRifle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INSURGENCY_API AINSWeapon_BoltRifle : public AINSWeaponBase
+{
+	GENERATED_BODY()
+	
+};
